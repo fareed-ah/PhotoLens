94ec12b8d6bbe8816636e0574a5ddfa6bd6995aa
Fareed Ahmad <fareedahmad@cmail.carleton.ca>
2020-12-25 21:21:00 -0500

    Setup navigation and material design libraries

--- PhotoLens/index.js
+++ PhotoLens/index.js
@@ -2,8 +2,22 @@
  * @format
  */
 
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import * as React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { Provider as PaperProvider } from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+    return (
+      <NavigationContainer>
+        <PaperProvider>
+            <App />
+        </PaperProvider>
+      </NavigationContainer>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
