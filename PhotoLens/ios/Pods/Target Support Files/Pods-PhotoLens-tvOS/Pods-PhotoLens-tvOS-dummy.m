19802afbd734073f2d5aa2e77bd053188bee7116
Fareed Ahmad <fareedahmad@cmail.carleton.ca>
2020-12-25 20:54:58 -0500

    init poject

---
+++ PhotoLens/ios/Pods/Target Support Files/Pods-PhotoLens-tvOS/Pods-PhotoLens-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhotoLens_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_PhotoLens_tvOS
+@end
---
+++ PhotoLens/ios/Pods/Target Support Files/Pods-PhotoLens/Pods-PhotoLens-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhotoLens : NSObject
+@end
+@implementation PodsDummy_Pods_PhotoLens
+@end
