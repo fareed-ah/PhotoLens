a96df43db2b33d534cda9c3f52de9f79e8fc47b4
Fareed Ahmad <fareedahmad@cmail.carleton.ca>
2021-01-07 15:45:02 -0500

    Refacter client side code

---
+++ client/index.js
@@ -0,0 +1,26 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import { DefaultTheme,Provider as PaperProvider } from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#428E7C',
+    accent: '#428E7C',
+  },
+};
+
+export default function Main() {
+    return (
+        <PaperProvider theme={theme}>
+            <App />
+        </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
